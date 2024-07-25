a2188df43669935b2c0ed30d1a779f9c0cfc5b30
es-ia-academy <165055180+es-ia-academy@users.noreply.github.com>
2024-07-25 10:44:49 +0200

    Update

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+  // Smooth scrolling para los enlaces del menú
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                const targetId = this.getAttribute('href');
+                const targetElement = document.querySelector(targetId);
+                const navHeight = document.querySelector('nav').offsetHeight;
+                const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - navHeight;
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: 'smooth'
+                });
+            });
+        });
