00bd13991195c9e011112ff0edb0488d444081be
es-ia-academy <165055180+es-ia-academy@users.noreply.github.com>
2024-07-25 11:44:10 +0200

    Update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,27 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
+        
         const targetId = this.getAttribute('href');
+        console.log(`Target ID: ${targetId}`);
+        
         const targetElement = document.querySelector(targetId);
-        if (!targetElement) return;
+        if (!targetElement) {
+            console.log(`Target element not found: ${targetId}`);
+            return;
+        }
+        console.log(`Target element found:`, targetElement);
+        
         const navSection = document.querySelector('section[data-type="navigation"]');
+        if (!navSection) {
+            console.log('Navigation section not found');
+        }
         const navHeight = navSection ? navSection.offsetHeight : 0;
+        console.log(`Navigation height: ${navHeight}`);
+        
         const targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - navHeight;
+        console.log(`Target position: ${targetPosition}`);
+        
         window.scrollTo({
             top: targetPosition,
             behavior: 'smooth'
